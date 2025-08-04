bcdf3f89b18cbb8609c40223f2aadb10d5d34b72
Anuj Basnet <anjbas123@gmail.com>
2025-08-05 01:13:30 +0545

    Implemented modal in Logout

--- screens/Tabs/MainSettings.jsx
+++ screens/Tabs/MainSettings.jsx
@@ -6,7 +6,7 @@ import {
   StatusBar,
   ScrollView,
   Pressable,
-  Modal
+  Modal,
 } from "react-native";
 import React, { useState } from "react";
 import { useFonts } from "expo-font";
@@ -18,6 +18,7 @@ import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 const { width, height } = Dimensions.get("window");
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
+import {BlurView} from 'expo-blur';
 
 const Settings = () => {
   const navigation = useNavigation();
@@ -29,7 +30,10 @@ const Settings = () => {
   const handleLogout = async () => {
     await AsyncStorage.removeItem("isLoggedIN");
   };
-  const [modalVisible, setmodalVisible] = useState(false)
+  const [modalVisible, setmodalVisible] = useState(false);
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={{ flex: 1 }}>
       <StatusBar barStyle={"dark-content"} />
@@ -75,200 +79,259 @@ const Settings = () => {
           />
         </Pressable>
         <Pressable onPress={() => navigation.navigate("ChangePassword")}>
-        <View style={styles.profileEdit}>
-          <View
-            style={{
-              marginLeft: width * 0.05,
-              flexDirection: "row",
-              gap: width * 0.03,
-            }}
-          >
-            <AntDesign
-              name="lock"
-              size={width * 0.05}
-              color="black"
-              style={{ marginTop: height * 0.003 }}
-            />
-            <Text
-              style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+          <View style={styles.profileEdit}>
+            <View
+              style={{
+                marginLeft: width * 0.05,
+                flexDirection: "row",
+                gap: width * 0.03,
+              }}
             >
-              Change Password
-            </Text>
-          </View>
-          <MaterialIcons
-            name="arrow-forward-ios"
-            size={24}
-            color="#c6c6c6ff"
-            style={{ marginRight: width * 0.05 }}
-          />
-        </View>
-        </Pressable>
-        <Pressable onPress={()=>{
-          navigation.navigate("ToggleNotification")
-        }}>
-        <View style={styles.profileEdit}>
-          <View
-            style={{
-              marginLeft: width * 0.05,
-              flexDirection: "row",
-              gap: width * 0.03,
-            }}
-          >
-            <Feather
-              name="bell"
-              size={width * 0.05}
-              color="black"
-              style={{ marginTop: height * 0.003 }}
-            />
-            <Text
-              style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
-            >
-              Notifications
-            </Text>
-          </View>
-          <MaterialIcons
-            name="arrow-forward-ios"
-            size={24}
-            color="#c6c6c6ff"
-            style={{ marginRight: width * 0.05 }}
-          />
-        </View>
-        </Pressable>
-        <Pressable onPress={()=>{
-          navigation.navigate("Security")
-        }}>
-        <View style={styles.profileEdit}>
-          <View
-            style={{
-              marginLeft: width * 0.05,
-              flexDirection: "row",
-              gap: width * 0.03,
-            }}
-          >
+              <AntDesign
+                name="lock"
+                size={width * 0.05}
+                color="black"
+                style={{ marginTop: height * 0.003 }}
+              />
+              <Text
+                style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+              >
+                Change Password
+              </Text>
+            </View>
             <MaterialIcons
-              name="security"
-              size={width * 0.05}
-              color="black"
-              style={{ marginTop: height * 0.003 }}
+              name="arrow-forward-ios"
+              size={24}
+              color="#c6c6c6ff"
+              style={{ marginRight: width * 0.05 }}
             />
-            <Text
-              style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
-            >
-              Security
-            </Text>
           </View>
-          <MaterialIcons
-            name="arrow-forward-ios"
-            size={24}
-            color="#c6c6c6ff"
-            style={{ marginRight: width * 0.05 }}
-          />
-        </View>
         </Pressable>
-        <Pressable onPress={()=>navigation.navigate('Language')}>
-        <View style={styles.profileEdit}>
-          <View
-            style={{
-              marginLeft: width * 0.05,
-              flexDirection: "row",
-              gap: width * 0.03,
-            }}
-          >
-            <SimpleLineIcons
-              name="globe"
-              size={width * 0.05}
-              color="black"
-              style={{ marginTop: height * 0.003 }}
-            />
-            <Text
-              style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+        <Pressable
+          onPress={() => {
+            navigation.navigate("ToggleNotification");
+          }}
+        >
+          <View style={styles.profileEdit}>
+            <View
+              style={{
+                marginLeft: width * 0.05,
+                flexDirection: "row",
+                gap: width * 0.03,
+              }}
             >
-              Language
-            </Text>
+              <Feather
+                name="bell"
+                size={width * 0.05}
+                color="black"
+                style={{ marginTop: height * 0.003 }}
+              />
+              <Text
+                style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+              >
+                Notifications
+              </Text>
+            </View>
+            <MaterialIcons
+              name="arrow-forward-ios"
+              size={24}
+              color="#c6c6c6ff"
+              style={{ marginRight: width * 0.05 }}
+            />
           </View>
-          <MaterialIcons
-            name="arrow-forward-ios"
-            size={24}
-            color="#c6c6c6ff"
-            style={{ marginRight: width * 0.05 }}
-          />
-        </View>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Security");
+          }}
+        >
+          <View style={styles.profileEdit}>
+            <View
+              style={{
+                marginLeft: width * 0.05,
+                flexDirection: "row",
+                gap: width * 0.03,
+              }}
+            >
+              <MaterialIcons
+                name="security"
+                size={width * 0.05}
+                color="black"
+                style={{ marginTop: height * 0.003 }}
+              />
+              <Text
+                style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+              >
+                Security
+              </Text>
+            </View>
+            <MaterialIcons
+              name="arrow-forward-ios"
+              size={24}
+              color="#c6c6c6ff"
+              style={{ marginRight: width * 0.05 }}
+            />
+          </View>
+        </Pressable>
+        <Pressable onPress={() => navigation.navigate("Language")}>
+          <View style={styles.profileEdit}>
+            <View
+              style={{
+                marginLeft: width * 0.05,
+                flexDirection: "row",
+                gap: width * 0.03,
+              }}
+            >
+              <SimpleLineIcons
+                name="globe"
+                size={width * 0.05}
+                color="black"
+                style={{ marginTop: height * 0.003 }}
+              />
+              <Text
+                style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+              >
+                Language
+              </Text>
+            </View>
+            <MaterialIcons
+              name="arrow-forward-ios"
+              size={24}
+              color="#c6c6c6ff"
+              style={{ marginRight: width * 0.05 }}
+            />
+          </View>
         </Pressable>
         <View style={{ marginTop: height * 0.02, marginLeft: width * 0.05 }}>
           <Text style={{ fontFamily: "Baloo2-Bold", fontSize: width * 0.052 }}>
             Preferencess
           </Text>
         </View>
-        <Pressable onPress={()=>{navigation.navigate('LegalPolicies')}}>
-        <View style={styles.profileEdit}>
-          <View
-            style={{
-              marginLeft: width * 0.05,
-              flexDirection: "row",
-              gap: width * 0.03,
-            }}
-          >
-            <Feather
-              name="shield"
-              size={width * 0.05}
-              color="black"
-              style={{ marginTop: height * 0.003 }}
-            />
-            <Text
-              style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+        <Pressable
+          onPress={() => {
+            navigation.navigate("LegalPolicies");
+          }}
+        >
+          <View style={styles.profileEdit}>
+            <View
+              style={{
+                marginLeft: width * 0.05,
+                flexDirection: "row",
+                gap: width * 0.03,
+              }}
             >
-              Legal and Policies
-            </Text>
+              <Feather
+                name="shield"
+                size={width * 0.05}
+                color="black"
+                style={{ marginTop: height * 0.003 }}
+              />
+              <Text
+                style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+              >
+                Legal and Policies
+              </Text>
+            </View>
+            <MaterialIcons
+              name="arrow-forward-ios"
+              size={24}
+              color="#c6c6c6ff"
+              style={{ marginRight: width * 0.05 }}
+            />
           </View>
-          <MaterialIcons
-            name="arrow-forward-ios"
-            size={24}
-            color="#c6c6c6ff"
-            style={{ marginRight: width * 0.05 }}
-          />
-        </View>
         </Pressable>
         <Pressable>
-        <View style={styles.profileEdit}>
-          <View
-            style={{
-              marginLeft: width * 0.05,
-              flexDirection: "row",
-              gap: width * 0.03,
-            }}
-          >
-            <MaterialIcons
-              name="support-agent"
-              size={width * 0.05}
-              color="black"
-              style={{ marginTop: height * 0.003 }}
-            />
-            <Text
-              style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+          <View style={styles.profileEdit}>
+            <View
+              style={{
+                marginLeft: width * 0.05,
+                flexDirection: "row",
+                gap: width * 0.03,
+              }}
             >
-              Help & Support
-            </Text>
+              <MaterialIcons
+                name="support-agent"
+                size={width * 0.05}
+                color="black"
+                style={{ marginTop: height * 0.003 }}
+              />
+              <Text
+                style={{ fontFamily: "Baloo2-Medium", fontSize: width * 0.04 }}
+              >
+                Help & Support
+              </Text>
+            </View>
+            <MaterialIcons
+              name="arrow-forward-ios"
+              size={24}
+              color="#c6c6c6ff"
+              style={{ marginRight: width * 0.05 }}
+            />
           </View>
-          <MaterialIcons
-            name="arrow-forward-ios"
-            size={24}
-            color="#c6c6c6ff"
-            style={{ marginRight: width * 0.05 }}
-          />
-        </View>
         </Pressable>
         <View style={styles.profileEdit}>
-          <Modal>
-
-            <Text>Hello</Text>
+          <Modal
+            transparent
+            animationType="none"
+            visible={modalVisible}
+            onRequestClose={() => {
+              handleLogout();
+              setmodalVisible(false);
+            }}
+          >
+            <BlurView intensity={10} tint="dark" style={styles.modalContainer}>
+              <View style={styles.modalView}>
+                <View>
+                  <Text style={styles.modalText}>Are you sure you want to </Text>
+                  <Text style={styles.modalText2}> logout?</Text>
+                </View>
+                <Pressable
+                  onPress={() => setmodalVisible(!modalVisible)}
+                  style={{
+                    backgroundColor: "#6C5DD3",
+                    height:height*0.07,
+                    width:width*0.4,
+                    alignItems:'center',
+                    justifyContent:'center',
+                    borderRadius:width*0.1,
+                    marginTop:height*0.02
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: "Baloo2-Medium",
+                      fontSize: width * 0.05,
+                      color: "#ffffff",
+                    }}
+                  >
+                    Cancel
+                  </Text>
+                </Pressable>
+                <Pressable onPress={handleLogout}>
+                  <Text
+                    style={{
+                      fontFamily: "Baloo2-Medium",
+                      fontSize: width * 0.05,
+                      color: "red",
+                      
+                    marginTop:height*0.02
+                    }}
+                  >
+                    Log Out
+                  </Text>
+                </Pressable>
+              </View>
+            </BlurView>
           </Modal>
           <Pressable
-            style={[{
-              marginLeft: width * 0.05,
-              flexDirection: "row",
-              gap: width * 0.03,
-            }]}
-            onPress={setmodalVisible(true)}
+            style={[
+              {
+                marginLeft: width * 0.05,
+                flexDirection: "row",
+                gap: width * 0.03,
+              },
+            ]}
+            onPress={() => setmodalVisible(true)}
           >
             <MaterialIcons
               name="logout"
@@ -312,5 +375,37 @@ const styles = StyleSheet.create({
     marginLeft: width * 0.03,
     height: height * 0.09,
     justifyContent: "space-between",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: "center",
+    paddingHorizontal: width * 0.05,
+    backfaceVisibility:'visible',
+  },
+  modalView: {
+    width: width * 0.85,
+    paddingVertical: height * 0.04,
+    paddingHorizontal: width * 0.06,
+    borderRadius: width * 0.04,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    elevation: 5, // for Android shadow
+    shadowColor: "#000", // for iOS shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  modalText: {
+    fontFamily: "Baloo2-Medium",
+    fontSize: Math.min(width, height) * 0.045,
+    textAlign: "center",
+    marginBottom: height * 0.015,
+  },
+  modalText2: {
+    fontFamily: "Baloo2-Medium",
+    fontSize: Math.min(width, height) * 0.045,
+    textAlign: "center",
   },
 });
